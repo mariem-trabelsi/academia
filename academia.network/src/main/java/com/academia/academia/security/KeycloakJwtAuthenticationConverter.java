ec290ec8f8c2a68a39f7b8f4770637b46356293a
mariem-trabelsi <mrtrabelsi547@gmail.com>
2025-03-29 13:26:31 +0100

    integrating auth provider : keycloak  ✨

---
+++ academia.network/src/main/java/com/academia/academia/security/KeycloakJwtAuthenticationConverter.java
@@ -0,0 +1,4 @@
+package com.academia.academia.security;
+
+public class KeycloakJwtAuthenticationConverter implements org.springframework.core.convert.converter.Converter<org.springframework.security.oauth2.jwt.Jwt, ? extends org.springframework.security.authentication.AbstractAuthenticationToken> {
+}
